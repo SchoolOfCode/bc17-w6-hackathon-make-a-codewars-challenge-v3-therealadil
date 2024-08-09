d23eb57c8d9606edb9c76a5b08ffffbae5a2e1ba
Adil <adilk23@outlook.com>
2024-08-09 09:36:59 +0100

    update tests

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 /**
- * Hello challenger! Your task is to write a function named `transformLength` which takes in a string and returns 1 if the length of the string is even and otherwise -1.
+ * Hello challenger! Your task is to write a function named `oddOrEven` which takes in a number and returns "odd" if the number is odd and "even" if the number is even.
  *
- * Your implementation should handle strings whose length is between 0 (inclusive) and 2500 (inclusive).
+ * Your implementation should handle numbers only passed into the function).
  *
  * A few examples:
- *    `transformLength("table")` should return -1 as the length of "table" is not even
- *    `transformLength("wizard")` should return 1 as the length of "wizard" is even
+ *    `oddOrEven(3)` should return "odd" as 3 is an odd number
+ *    `oddOrEven(4)` should return "even" as 4 is an even number
  */
 
 
-export function kata(num) {
-    if (num % 2 !== 0) {
+export function oddOrEven(number) {
+    if (number % 2 !== 0) {
         return "odd"
     } return "even"
     }
--- main.test.js
+++ main.test.js
@@ -1,11 +1,22 @@
 import {test, expect} from "vitest"
 
-import { kata } from "./main.js"
+import { oddOrEven } from "./main.js"
 
-test("kata odd", () => {
-    expect(kata(1)).toBe("odd")
+test("oddOrEven odd check", () => {
+    expect(oddOrEven(1)).toBe("odd")
+    expect(oddOrEven(3)).toBe("odd")
+    expect(oddOrEven(5)).toBe("odd")
+    expect(oddOrEven(7)).toBe("odd")
+    expect(oddOrEven(9)).toBe("odd")
+    expect(oddOrEven(11)).toBe("odd")
 })
 
-test("kata even", () => {
-    expect(kata(2)).toBe("even")
+test("oddOrEven even check", () => {
+    expect(oddOrEven(2)).toBe("even")
+    expect(oddOrEven(4)).toBe("even")
+    expect(oddOrEven(6)).toBe("even")
+    expect(oddOrEven(8)).toBe("even")
+    expect(oddOrEven(10)).toBe("even")
+    expect(oddOrEven(12)).toBe("even")
+    expect(oddOrEven(14)).toBe("even")
 })
